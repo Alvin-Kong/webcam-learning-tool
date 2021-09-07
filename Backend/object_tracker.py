2af313102e5cc7f1d3fe1e3bc0e7d614a20a89bf
Alvin Kong <axk1079@case.edu>
2021-09-07 09:31:02 -0400

    display the center of boundary box

--- Backend/object_tracker.py
+++ Backend/object_tracker.py
@@ -30,7 +30,17 @@ if __name__ == '__main__':
         p2 = (x + width, y + height)
         cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
 
-    # Loop to maintain video capture device input
+    #Function to get the center of boundary box
+    def getCenter(frame, bbox):
+        x = int(bbox[0])
+        y = int(bbox[1])
+        width = int(bbox[2])
+        height = int(bbox[3])
+        xCenter = x + (width / 2)
+        yCenter = y + (height /2)
+        cv2.putText(frame, "Center: " + str(int(xCenter)) + ", " + str(int(yCenter)), (0, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+    # Loop to maintain video capture device input and display
     while(capture.isOpened()):
         timer = cv2.getTickCount()
         ret, frame = capture.read()
@@ -41,19 +51,24 @@ if __name__ == '__main__':
         
         # Update the tracker every frame
         ret, bbox = tracker.update(frame)
+        
+        # Print the position of the boundary box
+        #print(bbox)
 
         # Display 'Tracking' if tracking is successful and 'Lost' if tracking is unsuccessful
         if ret:
             drawObjectBox(frame, bbox)
-            cv2.putText(frame, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+            cv2.putText(frame, "Tracking", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
         else:
-            cv2.putText(frame, "Lost", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+            cv2.putText(frame, "Lost", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         # Calculate and display frames per second (FPS)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-        cv2.putText(frame, "FPS : " + str(int(fps)), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+        cv2.putText(frame, "FPS : " + str(int(fps)), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
+        # Display the center of the boundary box
+        getCenter(frame, bbox)
 
         # Change video capture input to grayscale
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
