f3986d88bdf2f181729ed5b8a2e120a11d108d12
Rbower123 <Reedlbower@gmail.com>
2021-10-26 22:29:50 -0400

    removing outlined object from the traced image

--- Backend/tracing.py
+++ Backend/tracing.py
@@ -3,21 +3,27 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    img = cv2.imread("/Users/reedb/OneDrive/Desktop/index.png")
-    orig = copy(img)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    original = cv2.imread("/Users/reedb/OneDrive/Desktop/index.png")
+    traced = cv2.imread("/Users/reedb/OneDrive/Desktop/indexTraced.png")
+    tracedOriginal = copy(traced)
+    gray = cv2.cvtColor(traced, cv2.COLOR_BGR2GRAY)
 
-    threshold_level = 10
+    threshold_level = 30
     coords = np.column_stack(np.where(gray < threshold_level))
 
     print(coords)
 
     mask = gray < threshold_level
 
+    traced[mask] = (255, 255, 255)
 
-    img[mask] = (204, 119, 0)
-
-    cv2.imshow('image', img)
-    cv2.imshow('original', orig)
+    cv2.imshow('original', original)
+    cv2.imshow('traced', traced)
+    cv2.imshow('tracedOriginal', tracedOriginal)
 
     cv2.waitKey(0)
+
+
+
+
+
