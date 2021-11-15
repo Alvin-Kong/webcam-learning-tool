6225996acc840e39ef8625e01f1ab0029906f6fe
Alvin Kong <axk1079@case.edu>
2021-11-14 23:11:33 -0500

    working api with object tracking

--- Backend/api.py
+++ Backend/api.py
@@ -2,15 +2,18 @@ import flask
 from flask import request
 from flask_cors import CORS
 
+import cv2
 import asyncio
 import json
 import sys
+import threading
 
 import util
 
 app = flask.Flask(__name__)
 cors = CORS(app)
 
+objectTracker = object()
 
 # Main function to respond to requests
 @app.route("/", methods=["GET"])
@@ -27,14 +30,14 @@ async def build_response():
     return response
 
 
-# Function to ge the
+# Function to build a json response containing the coordinates of the boundary box
 async def get_position():
     response = {}
-    bbox = util.getBBox()
-    response["center"] = util.getCenter(bbox)
-    response["x"] = util.getXCenter(bbox)
-    response["y"] = util.getYCenter(bbox)
-    return response.json()
+    response["center"] = objectTracker.getCenter()
+    response["x"] = objectTracker.getXCenter()
+    response["y"] = objectTracker.getYCenter()
+    print(response)
+    return json.dumps(response)
 
 
 # Generate an JSON error response
@@ -49,6 +52,175 @@ def generateError(reason):
     }
 
 
+# Class for the object tracker
+class ObjectTracker():
+    def __init__(self, capture, ret, frame, bbox, tracker, draw):
+        self._capture = capture
+        self._ret = ret
+        self._frame = frame
+        self._bbox = bbox
+        self._tracker = tracker
+        self._draw = draw
+        self._points = []
+
+           
+    # Function to maintain a rectangle to define the boundary box around the tracked object 
+    def drawBoundaryBox(self, frame, bbox):
+        x = int(bbox[0])
+        y = int(bbox[1])
+        width = int(bbox[2])
+        height = int(bbox[3])
+        p1 = (x, y)
+        p2 = (x + width, y + height)
+        cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+
+
+    # Function to get the center of boundary box
+    def getCenter(self):
+        xcenter = self.getXCenter()
+        ycenter = self.getYCenter()
+        return xcenter, ycenter
+
+
+    # Function to get the x coordinate of the center of the boundary box
+    def getXCenter(self):
+        x = int(self._bbox[0])
+        width = int(self._bbox[2])
+        xCenter = x + (width / 2)
+        return xCenter
+
+
+    # Function to get the y coordinate of the center of the boundary box
+    def getYCenter(self):
+        y = int(self._bbox[1])
+        height = int(self._bbox[3])
+        yCenter = y + (height / 2)
+        return yCenter
+
+
+    # Function to build a json response containing the coordinates of the boundary box
+    async def get_position(self):
+        response = {}
+        # response["center"] = self.getCenter()
+        response["x"] = self.getXCenter()
+        response["y"] = self.getYCenter()
+        print(response)
+        return json.dumps(response)
+
+
+    # Main method to run object tracker
+    def runTracker(self):
+        # Loop to maintain video capture device input and display
+        while(self._capture.isOpened()):
+            timer = cv2.getTickCount() 
+            self._ret, self._frame = self._capture.read()
+            
+            # ret will return 'True' if video caputre is good and 'False' if video capture is bad
+            if not self._ret:
+                break
+
+            # Update the tracker every frame
+            self._ret, self._bbox = self._tracker.update(self._frame)
+
+            # Display 'Tracking' if tracking is successful and 'Lost' if tracking is unsuccessful
+            if self._ret:
+                self.drawBoundaryBox(self._frame, self._bbox)
+                cv2.putText(self._frame, "Tracking", (0, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+            else:
+                cv2.putText(self._frame, "Lost", (0, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+
+            # Calculate and display frames per second (FPS)
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            cv2.putText(self._frame, "FPS : " + str(int(fps)), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+
+            # Draw lines according to the position of the center of the boundary box and display the center of the boundary box
+            if self._draw == True:
+                # xcenter, ycenter = util.getCenter(bbox)
+                xcenter = self.getXCenter()
+                ycenter = self.getYCenter()
+                # print(xcenter, ",", ycenter)
+                cv2.putText(self._frame, "Center: " + str(int(xcenter)) + ", " + str(int(ycenter)), (0, 125), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+                cv2.putText(self._frame, "Drawing", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+                
+            else:
+                cv2.putText(self._frame, "Stopped", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0), 2)
+
+            # Change video capture input to grayscale
+            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            
+            # Display video capture
+            #cv2.imshow('object-tracker', gray)
+            cv2.imshow('object-tracker', self._frame)
+
+            # Toggle drawing (activation of drawing pen)
+            if cv2.waitKey(1) & 0xFF == ord(' '):
+                if self._draw == True:
+                    self._draw = False
+                else:
+                    self._draw = True
+
+            # Press 'esc' key to exit
+            if cv2.waitKey(1) & 0xFF == 27:
+                # Kill all video capture processes and displays
+                self._capture.release()
+                cv2.destroyAllWindows()
+
+    
+    @property
+    def capture(self):
+        return self._capture
+
+    def ret(self):
+        return self._ret
+
+    def frame(self):
+        return self._frame
+
+    def bbox(self):
+        return self._bbox
+
+    def tracker(self):
+        return self._tracker
+
+    def draw(self):
+        return self._draw
+
+    def points(self):
+        return self._points
+
+
+def initializeTracker():
+    draw = False
+
+    # Declare video capture device (webcam)
+    capture = cv2.VideoCapture(0)
+
+    # Check video capture device works
+    if not capture.isOpened():
+        print("Could not open video capture device")
+        sys.exit()
+
+    # Declare the type of tracking used
+    tracker = cv2.TrackerCSRT_create()
+
+    # Read the first frame and initialize the boundary box for the tracker
+    ret, frame = capture.read()
+    bbox = cv2.selectROI(frame, False)
+    tracker.init(frame, bbox)
+
+    return capture, ret, frame, bbox, tracker, draw
+
+
+def getTracker():
+    return objectTracker
+
+
+# Function to run the API
+def runAPI(DEBUG, PORT, SSL_CONTEXT):
+    app.config["DEBUG"] = DEBUG
+    app.run(port=PORT, ssl_context=SSL_CONTEXT)
+ 
+
 # Main method to run API
 if __name__ == "__main__":
     PORT = 5000  # port to run the server on
@@ -61,5 +233,16 @@ if __name__ == "__main__":
     if "--https" in sys.argv:
         SSL_CONTEXT = "ahdoc"
 
-    app.config["DEBUG"] = DEBUG
-    app.run(port=PORT, ssl_context=SSL_CONTEXT)
+    capture, ret, frame, bbox, tracker, draw = initializeTracker()
+    objectTracker = ObjectTracker(capture, ret, frame, bbox, tracker, draw)
+    trackerThread = threading.Thread(target=objectTracker.runTracker)
+
+    apiThread = threading.Thread(target=runAPI, args=(DEBUG, PORT, SSL_CONTEXT,))
+
+    apiThread.start()
+    trackerThread.start()
+
+    apiThread.join()
+    trackerThread.join()
+
+    print("Program Terminated")
--- Backend/object_tracker.py
+++ Backend/object_tracker.py
@@ -1,9 +1,7 @@
 import cv2
 import sys
 import util
-
-frame = object()
-bbox = object()
+import numpy as np
 
 
 # Function to draw a rectangle to define the boundary box
@@ -17,37 +15,18 @@ def drawObjectBox(frame, bbox):
     cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
 
 
-def getFrame():
-    return frame
+def printCords(bbox):
+    xcenter = util.getXCenter(bbox)
+    ycenter = util.getYCenter(bbox)
+    print(xcenter, ",", ycenter)
+    # get_center.getCenter()
+    # print(getBBox())
 
 
-def getBBox():
-    return getBBox
-
-
-# Main running function
-if __name__ == '__main__':
-    draw = False
-
-    # Declare video capture device (webcam)
-    capture = cv2.VideoCapture(0)
-
-    # Check video capture device works
-    if not capture.isOpened():
-        print("Could not open video capture device")
-        sys.exit()
-
-    # Declare the type of tracking used
-    tracker = cv2.TrackerCSRT_create()
-
-    # Read the first frame and initialize the boundary box for the tracker
-    ret, frame = capture.read()
-    bbox = cv2.selectROI(frame, False)
-    tracker.init(frame, bbox)
-
+def runTracker(capture, ret, frame, bbox, tracker, draw):
     # Loop to maintain video capture device input and display
     while(capture.isOpened()):
-        timer = cv2.getTickCount()
+        timer = cv2.getTickCount() 
         ret, frame = capture.read()
         
         # ret will return 'True' if video caputre is good and 'False' if video capture is bad
@@ -78,8 +57,11 @@ if __name__ == '__main__':
             ycenter = util.getYCenter(bbox)
             cv2.putText(frame, "Center: " + str(int(xcenter)) + ", " + str(int(ycenter)), (0, 125), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
             cv2.putText(frame, "Drawing", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+            
         else:
             cv2.putText(frame, "Stopped", (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0), 2)
+
+            bbox = util.getBBox()
 
         # Change video capture input to grayscale
         #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -97,8 +79,34 @@ if __name__ == '__main__':
 
         # Press 'esc' key to exit
         if cv2.waitKey(1) & 0xFF == 27:
-            break
+            # Kill all video capture processes and displays
+            capture.release()
+            cv2.destroyAllWindows()
 
-    # Kill all video capture processes and displays
-    capture.release()
-    cv2.destroyAllWindows()
+
+def initialize():
+    draw = False
+
+    # Declare video capture device (webcam)
+    capture = cv2.VideoCapture(0)
+
+    # Check video capture device works
+    if not capture.isOpened():
+        print("Could not open video capture device")
+        sys.exit()
+
+    # Declare the type of tracking used
+    tracker = cv2.TrackerCSRT_create()
+
+    # Read the first frame and initialize the boundary box for the tracker
+    ret, frame = capture.read()
+    bbox = cv2.selectROI(frame, False)
+    tracker.init(frame, bbox)
+
+    return capture, ret, frame, bbox, tracker, draw
+
+
+# Main running function
+if __name__ == '__main__':
+    capture, ret, frame, bbox, tracker, draw = initialize()
+    runTracker(capture, ret, frame, bbox, tracker, draw)
